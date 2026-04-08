33eafe5693b7a44730e5007997a01960884043df
Eve-LynBremner <emvanschoor@gmail.com>
2026-04-08 20:45:15 +0100

    update about page and add styling

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,13 +1,27 @@
-// HomePage.js
+// AboutPage.js
 import React from 'react';
+import styles from './AboutPage.module.css';
 
-const HomePage = () => {
+const AboutPage = () => {
   return (
-    <div>
-      <h2>Welcome to the About Page</h2>
-      <p>This is the main content of the about page.</p>
+    <div className={styles.container}>
+      <h2 className={styles.heading}>Welcome to the About Page</h2>
+      <p className={styles.content}>I’m an analytical problem‑solver with a background in financial services, 
+        where I learned to turn data, patterns, and complex information into clear insights. 
+        Working with SQL, Excel, and reporting tools sharpened my logic, testing mindset, 
+        and love for structured problem‑solving.
+        
+        That curiosity naturally led me into software development. 
+        I now combine my analytical experience with hands‑on skills in Python, 
+        HTML, CSS, JavaScript, React, and Node.js, strengthened through a 
+        BCS‑accredited Full Stack Web Development bootcamp.
+        
+        I’m comfortable in Agile environments and familiar with tools like Jira, 
+        Confluence, and SharePoint. As an aspiring Software Developer and Tester, 
+        I’m passionate about clean logic, intuitive design, and building solutions 
+        that genuinely make life easier.</p>
     </div>
   );
 };
 
-export default HomePage;
+export default AboutPage;
