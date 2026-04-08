c6a4b976e0ac22d6d8c997feb4cba43cc2d3e722
Eve-LynBremner <emvanschoor@gmail.com>
2026-04-08 16:10:42 +0100

    add projects page to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import { useState } from 'react';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import ProjectsPage from './pages/ProjectsPage';
 import ContactPage from './pages/ContactPage';
 
 function App() {
 
   const [page, setPage] = useState('home');
 
-  // TODO: How doe sthe Layout component know which page to render?
-
   return (
     <Layout selectedPage={page} onSetPage={setPage}>
 
       {page === 'home' && <HomePage />}
       {page === 'about' && <AboutPage />}
+      {page === 'projects' && <ProjectsPage />}
       {page === 'contact' && <ContactPage />}
+      
 
     </Layout>
   );
