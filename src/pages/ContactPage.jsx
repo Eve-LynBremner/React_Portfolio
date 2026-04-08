c93b44f3b1b57ad10f2a603827d0f9aef9923aab
Eve-LynBremner <emvanschoor@gmail.com>
2026-04-08 21:23:05 +0100

    update contact page and add styling

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,24 @@
 // ContactPage.js
 import React from 'react';
+import styles from './ContactPage.module.css';
 
 const ContactPage = () => {
   return (
-    <div>
-      <h2>Welcome to the Contact Page</h2>
-      <p>This is the main content of the contact page.</p>
+    <div className={styles.container}>
+      <h2 className={styles.heading}>Get in Touch</h2>
+      {/* HMTL form converted into jsx format */}
+      <form className={styles.form}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name_input" id="name" />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email_input" id="email" />
+
+        <label htmlFor="message">Message:</label>
+        <textarea name="message_input" id="message" rows="15"></textarea>
+
+        <button className="button sub" type="submit">Submit</button>
+      </form>
     </div>
   );
 };
