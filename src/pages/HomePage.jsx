5bdd4245a4ae4f912b6d9912e2feb0e1beb79323
Eve-LynBremner <emvanschoor@gmail.com>
2026-04-09 03:22:34 +0100

    and profile pic and logoloop from react bits, to the homepage. update styling. Still need to add repo list

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,30 @@
 // HomePage.js
 import React from 'react';
 
+import profile_pic from '../assets/profile_pic.jpg';
+import styles from './HomePage.module.css';
+
+import RepoLoop from '../components/RepoLoop';
+
+
+// adding the logo loop from react bits but inserting github repos
 const HomePage = () => {
   return (
     <div>
-      <h2>Welcome to the Home Page</h2>
-      <p>This is the main content of the home page.</p>
+      <div className={styles.container}>
+        <img className={styles.image} src={profile_pic} alt="profile pic" />
+      </div>
+      <div className={styles.loopcontainer}>
+        <RepoLoop
+          repos={myRepos}
+          speed={70}
+          direction="down"
+          logoHeight={70}
+          gap={60}
+          hoverSpeed={0}
+          useCustomRender={false}
+        />
+      </div>
     </div>
   );
 };
