65356a44b1dd4e985e14ce3913267e274d5d8738
Eve-LynBremner <emvanschoor@gmail.com>
2026-04-09 08:50:12 +0100

    update ChromaGrid components to have title sit on top of image

--- src/components/ChromaGrid.jsx
+++ src/components/ChromaGrid.jsx
@@ -154,13 +154,13 @@ export const ChromaGrid = ({
         >
           <div className="chroma-img-wrapper">
             <img src={c.image} alt={c.title} loading="lazy" />
+            <div className="text-on-top">
+              <h3 className="name">{c.title}</h3>
+              {c.handle && <span className="handle">{c.handle}</span>}
+              <p className="role">{c.subtitle}</p>
+              {c.location && <span className="location">{c.location}</span>}
+            </div>
           </div>
-          <footer className="chroma-info">
-            <h3 className="name">{c.title}</h3>
-            {c.handle && <span className="handle">{c.handle}</span>}
-            <p className="role">{c.subtitle}</p>
-            {c.location && <span className="location">{c.location}</span>}
-          </footer>
         </article>
       ))}
       <div className="chroma-overlay" />
