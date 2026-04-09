7540620fc5bd065ec9f65b8976c6b2ada1096f3d
Eve-LynBremner <emvanschoor@gmail.com>
2026-04-09 01:24:25 +0100

    added floating lines to layout component, styled this to be the background. and styled the original content to be the foreground

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,8 @@ import NavBar from './NavBar'
 import Header from './Header'
 import Footer from './Footer';
 
+import FloatingLines from "../components/FloatingLines";
+
 const pages = [
   { name: 'Home', key: 'home' },
   { name: 'About', key: 'about' },
@@ -13,25 +15,42 @@ const Layout = ({ children, selectedPage, onSetPage}) => {
 
   return (
     <div style={styles.container}>
-      {/* Header */}
-      <Header />
 
-      {/* Navigation */}
-      <NavBar 
-        pages={pages}
-        selectedPage={selectedPage}
-        onSetPage={onSetPage}
-      />
-
-      <div style={styles.main}>
-        {/* Content Area */}
-        <section style={styles.content}>
-          {children}
-        </section>
+      {/* BACKGROUND */}
+      <div style = {{position: "absolute", inset: 0, zIndex: 0}}>
+        <FloatingLines
+          enabledWaves={["top", "middle", "bottom"]}
+          lineCount={5}
+          lineDistance={5}
+          bendRadius={5}
+          bendStrength={-0.5}
+          interactive={true}
+          parallax={true}
+        />
       </div>
 
-      {/* Footer */}
-      <Footer />
+      {/* FOREGROUND */}
+      <div style = {{position: "relative", zIndex: 2, display: "flex", flexDirection: "column", minHeight: "100vh"}}>
+        {/* Header */}
+        <Header />
+
+        {/* Navigation */}
+        <NavBar 
+          pages={pages}
+          selectedPage={selectedPage}
+          onSetPage={onSetPage}
+        />
+
+        <div style={styles.main}>
+          {/* Content Area */}
+          <section style={styles.content}>
+            {children}
+          </section>
+        </div>
+
+        {/* Footer */}
+        <Footer />
+      </div>
      
     </div>
   );
@@ -43,7 +62,7 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     minHeight: '100vh',
-    minWidth: '100vw',
+    minWidth: '100vw'
   },
   main: {
     display: 'flex',
